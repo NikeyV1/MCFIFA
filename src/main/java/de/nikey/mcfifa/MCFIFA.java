d3f0f5e039d9cdb04dd8822f0a4f7652bedb9edb
NikeV1 <n.heidorn@outlook.de>
2023-08-28 18:41:36 +0200

    Update

--- src/main/java/de/nikey/mcfifa/MCFIFA.java
+++ src/main/java/de/nikey/mcfifa/MCFIFA.java
@@ -5,11 +5,11 @@ import de.nikey.mcfifa.events.ModEvents;
 import de.nikey.mcfifa.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -17,6 +17,9 @@ import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.level.NoteBlockEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -29,6 +32,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.checkerframework.checker.units.qual.C;
 import org.slf4j.Logger;
 
 @Mod(MCFIFA.MODID)
@@ -36,6 +40,8 @@ public class MCFIFA {
 
     public static final String MODID = "mcfifa";
     private static final Logger LOGGER = LogUtils.getLogger();
+    private int sprintTickCounter = 0;
+    private int times;
 
     public MCFIFA() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -66,5 +72,47 @@ public class MCFIFA {
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         LOGGER.info("Mod starting!");
+    }
+
+    @SubscribeEvent
+    public void onLivingUpdate(LivingEvent.LivingTickEvent event) {
+        LivingEntity entity = event.getEntity();
+
+        // Überprüfe, ob der Spieler Diamantschuhe trägt und sprintet
+        if (entity.getItemBySlot(net.minecraft.world.entity.EquipmentSlot.FEET).getItem().equals(ModItems.FOOTBALL_BOOTS) && entity instanceof Player) {
+            Player player = (Player) entity;
+            player.sendSystemMessage(Component.literal("tick"));
+            if (entity.isSprinting()){
+                sprintTickCounter++;
+                if (sprintTickCounter >= 20) {
+                    times++;
+                    sprintTickCounter = 0;
+                    player.sendSystemMessage(Component.literal("You speed increased!"));
+                    if (times == 1){
+                        increaseSprintSpeed(player,1.2f);
+                    } else if (times == 2){
+                        increaseSprintSpeed(player,1.5f);
+                    } else if (times == 3) {
+                        increaseSprintSpeed(player,1.7f);
+                    }else if (times == 4){
+                        times=0;
+                        increaseSprintSpeed(player,2.0f);
+                    }
+                }
+            }else {
+                sprintTickCounter = 0;
+            }
+
+        }else{
+            if (entity instanceof Player){
+                Player player = (Player) entity;
+                player.getAbilities().setWalkingSpeed(0.1f);
+            }
+        }
+    }
+    private void increaseSprintSpeed(Player player,float f) {
+        // Erhöhen Sie die Geschwindigkeit des Spielers um 10% (Multiplikation mit 1.1)
+        float newSpeed = player.getAbilities().getWalkingSpeed() * 1.1f;
+        player.getAbilities().setWalkingSpeed(newSpeed);
     }
 }
--- src/main/java/de/nikey/mcfifa/events/ModEvents.java
+++ src/main/java/de/nikey/mcfifa/events/ModEvents.java
@@ -3,49 +3,15 @@ package de.nikey.mcfifa.events;
 import de.nikey.mcfifa.MCFIFA;
 import de.nikey.mcfifa.item.ModItems;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = MCFIFA.MODID , bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEvents {
     public static class ForgeEvents{
 
-        @SubscribeEvent
-        public void onLivingUpdate(LivingEvent.LivingTickEvent event) {
-            if (event.getEntity() instanceof net.minecraft.server.level.ServerPlayer) {
-                net.minecraft.server.level.ServerPlayer player = (net.minecraft.server.level.ServerPlayer) event.getEntity();
-                player.sendSystemMessage(Component.literal("jup"));
-                ItemStack boots = player.getItemBySlot(net.minecraft.world.entity.EquipmentSlot.FEET);
 
-                if (!boots.isEmpty() && boots.getItem().equals(ModItems.FOOTBALL_BOOTS) && player.isSprinting()) {
-                    player.sendSystemMessage(Component.literal("Gay"));
-                }
-            }
-        }
-
-        @SubscribeEvent
-        public static void onTick(TickEvent.PlayerTickEvent event){
-            event.player.sendSystemMessage(Component.literal("Gay"));
-            if (event.side == LogicalSide.SERVER){
-                Player player = event.player;
-                player.sendSystemMessage(Component.literal("server"));
-                if (player.hasItemInSlot(EquipmentSlot.FEET)){
-                    if (player.getInventory().getArmor(8).getItem().equals(ModItems.FOOTBALL_BOOTS)){
-                        player.sendSystemMessage(Component.literal("yes sir"));
-                    }else {
-                        player.sendSystemMessage(Component.literal("no"));
-                    }
-                }
-            }
-        }
     }
 }
